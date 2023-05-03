102ebb4dff5503a9dd68accb7e8768c3c5cbb973
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-03 14:54:14 +0200

    desktop styling

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,110 +1,138 @@
-import { createStyles, getStylesRef, Navbar } from '@mantine/core';
-import { useState } from 'react';
-
+import { createStyles, getStylesRef, Navbar } from "@mantine/core";
+import { useState } from "react";
+import clip from '../assets/clip.png';
 
 
 const useStyles = createStyles((theme) => ({
-    button: {
-        margin: theme.spacing.sm,
-        textTransform: 'uppercase',
-    },
+  button: {
+    margin: theme.spacing.sm,
+    textTransform: "uppercase",
+  },
 
-    wrapper: {
-        backgroundColor: theme.colors.orange[5],
-      },
+  wrapper: {
+    backgroundColor: theme.colors.orange[5],
+  },
 
   footer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: theme.spacing.xl,
     marginTop: theme.spacing.md,
-    
   },
 
   link: {
     ...theme.fn.focusStyles(),
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: theme.colors.cyan[3],
-    textDecoration: 'none',
+    textDecoration: "none",
     fontSize: theme.fontSizes.sm,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[1]
+        : theme.colors.gray[7],
     padding: `${theme.spacing.lg} ${theme.spacing.lg}`,
     borderRadius: theme.radius.xl,
     fontWeight: 500,
-    textTransform: 'uppercase',
+    textTransform: "uppercase",
     margin: theme.spacing.sm,
 
-    
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
 
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-
-      [`& .${getStylesRef('icon')}`]: {
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      [`& .${getStylesRef("icon")}`]: {
+        color: theme.colorScheme === "dark" ? theme.white : theme.black,
       },
     },
   },
 
   link2: {
     ...theme.fn.focusStyles(),
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: theme.colors.yellow[5],
-    textDecoration: 'none',
+    textDecoration: "none",
     fontSize: theme.fontSizes.sm,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[1]
+        : theme.colors.gray[7],
     padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
     borderRadius: theme.radius.xl,
     fontWeight: 500,
 
-    '&:hover': {
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-  
-        [`& .${getStylesRef('icon')}`]: {
-          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-        },
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
+
+      [`& .${getStylesRef("icon")}`]: {
+        color: theme.colorScheme === "dark" ? theme.white : theme.black,
       },
-},
+    },
+  },
+
+  image: {
+    position: "absolute",
+    zIndex: 1,
+    top: 665, // Adjust this value to position the image
+    left: 130, // Adjust this value to position the image
+    width: '3rem',
+    height: '3rem',
+  },
 
   linkIcon: {
-    ref: getStylesRef('icon'),
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+    ref: getStylesRef("icon"),
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[2]
+        : theme.colors.gray[6],
     marginRight: theme.spacing.sm,
   },
 
   linkActive: {
-    '&, &:hover': {
-      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
-      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-      [`& .${getStylesRef('icon')}`]: {
-        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+    "&, &:hover": {
+      backgroundColor: theme.fn.variant({
+        variant: "light",
+        color: theme.primaryColor,
+      }).background,
+      color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
+        .color,
+      [`& .${getStylesRef("icon")}`]: {
+        color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
+          .color,
       },
     },
   },
 }));
 
 interface DataItem {
-    link: string;
-    label: string;
+  link: string;
+  label: string;
 }
 
 interface NavBarProps {
-    data: DataItem[];
+  data: DataItem[];
 }
 
-export function NavbarSimple({ data }:NavBarProps) {
+export function NavbarSimple({ data }: NavBarProps) {
   const { classes, cx } = useStyles();
-  const [active, setActive] = useState('Billing');
+  const [active, setActive] = useState("Billing");
 
   const links = data.map((item: DataItem) => (
     <a
-      className={cx(classes.link, { [classes.linkActive]: item.label === active })}
+      className={cx(classes.link, {
+        [classes.linkActive]: item.label === active,
+      })}
       href={item.link}
       key={item.label}
       onClick={(event) => {
@@ -118,12 +146,14 @@ export function NavbarSimple({ data }:NavBarProps) {
 
   return (
     <Navbar className={classes.wrapper} height={700} width={{ sm: 300 }} p="md">
-      <Navbar.Section grow>
-
-        {links}
-      </Navbar.Section>
+      <Navbar.Section grow>{links}</Navbar.Section>
       <Navbar.Section className={classes.footer}>
-        <a href="#" className={classes.link2} onClick={(event) => event.preventDefault()}>
+        <a
+          href="#"
+          className={classes.link2}
+          onClick={(event) => event.preventDefault()}
+        >
+            <img src={clip} alt="Clip" className={classes.image}/>
           <span className={classes.button}>Create New Room</span>
         </a>
       </Navbar.Section>
