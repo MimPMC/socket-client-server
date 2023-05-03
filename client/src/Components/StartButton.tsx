092c3c4e54fcd2227740c25a4fa5462d24a01bda
mims <113519976+MimPMC@users.noreply.github.com>
2023-05-03 14:42:40 +0200

    loginpage styling

---
+++ client/src/Components/StartButton.tsx
@@ -0,0 +1,33 @@
+import { Button, createStyles } from '@mantine/core';
+
+export const useStyles = createStyles((theme) => ({
+  button1: {
+    background: "#54FFF5",
+    color: "black",
+    fontSize: "1.3rem",
+    transition: "all 0.3s ease",
+    cursor: "pointer",
+
+    "&:hover": {
+      background: "#5bfaef",
+    },
+
+    "&:active": {
+      transform: "scale(0.95)",
+    },
+
+    [theme.fn.smallerThan('sm')]: {
+      width: "70%",
+    },
+  },
+}));
+
+export function StartButton() {
+  const { classes } = useStyles();
+
+  return (
+    <Button radius="xl" size="md" w={"20rem"} className={classes.button1}>
+      Start Chatting!
+    </Button>
+  );
+}
--- client/src/pages/LogInPage.tsx
+++ client/src/pages/LogInPage.tsx
@@ -1,7 +1,66 @@
+import { Box, Container, Flex, Image, Title, createStyles } from "@mantine/core";
+import { NameInput } from "../Components/NameInput";
+import { StartButton } from "../Components/StartButton";
+
+const useStyles = createStyles((theme) => ({
+    test: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: `${theme.spacing.md} ${theme.spacing.md}`,
+  
+      [theme.fn.smallerThan('sm')]: {
+        flexDirection: 'column',
+      },
+    },
+  
+    links: {
+      [theme.fn.smallerThan('sm')]: {
+        marginTop: theme.spacing.lg,
+        marginBottom: theme.spacing.sm,
+      },
+    },
+    container: {
+        display: "flex",    
+        fontFamily: "'Gaegu', cursive",
+        flexDirection: "column",
+        alignContent: "center",
+        justifyContent:"center",
+        [theme.fn.smallerThan('sm')]: {
+            justifyContent:"center",
+
+
+          },
+
+    },
+    text: {
+        fontFamily: "'Gaegu', cursive",
+
+    }
+  }));
+  
+  
+
 export function LogInPage() {
+    const { classes } = useStyles();
+
     return(
-        <div>
-            <h1>hello homepage</h1>
-        </div>
+        <Box w={"100%"} bg={"#FD7E14"} h={"100svh"}>
+            <Container size="xl" px={0} bg={"#ffcfa7"} h={"100svh"} className={classes.container}>
+                <Flex direction="column" align={"center"} mb={10}>
+                    <Image maw={240} mx="auto" radius="100%" src="./src/assets/cat.jpg" alt="Random image" />
+                    <Title order={1} className={classes.text} weight={700} size={"3rem"}>Cat Chat</Title>
+                </Flex>
+                <Flex direction="column" align={"center"} gap={20} >
+                  <NameInput></NameInput>
+                  <StartButton></StartButton>
+                    
+                </Flex>
+            
+            </Container>
+
+        </Box>
+        
     )
 }
+
