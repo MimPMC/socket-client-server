406b88295b841efc76859c49cdb2033138d20281
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-03 13:53:23 +0200

    blue buttons

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import { createStyles, Navbar, getStylesRef, rem } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    paddingBottom: theme.spacing.md,
+    marginBottom: `calc(${theme.spacing.md} * 1.5)`,
+    borderBottom: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
+  },
+
+  footer: {
+    paddingTop: theme.spacing.md,
+    marginTop: theme.spacing.md,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
+  },
+
+  link: {
+    ...theme.fn.focusStyles(),
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: theme.colors.cyan[2],
+    textDecoration: 'none',
+    fontSize: theme.fontSizes.sm,
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
+    borderRadius: theme.radius.xl,
+    fontWeight: 500,
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+      [`& .${getStylesRef('icon')}`]: {
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      },
+    },
+  },
+
+  linkIcon: {
+    ref: getStylesRef('icon'),
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+    marginRight: theme.spacing.sm,
+  },
+
+  linkActive: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      [`& .${getStylesRef('icon')}`]: {
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      },
+    },
+  },
+}));
+
+interface DataItem {
+    link: string;
+    label: string;
+}
+
+interface NavBarProps {
+    data: DataItem[];
+}
+
+export function NavbarSimple({ data }:NavBarProps) {
+  const { classes, cx } = useStyles();
+  const [active, setActive] = useState('Billing');
+
+  const links = data.map((item: DataItem) => (
+    <a
+      className={cx(classes.link, { [classes.linkActive]: item.label === active })}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <span>{item.label}</span>
+    </a>
+  ));
+
+  return (
+    <Navbar height={700} width={{ sm: 300 }} p="md">
+      <Navbar.Section grow>
+        {links}
+      </Navbar.Section>
+    </Navbar>
+  );
+}
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,18 @@
+import { NavbarSimple } from "../components/Sidebar";
+
+const data = [
+    { link: '', label: 'Room 1', },
+    { link: '', label: 'Room 2',  },
+    { link: '', label: 'Room 3',  },
+    { link: '', label: 'Room 4',  },
+
+  ];
+
 export function HomePage() {
     return(
         <div>
             <h1>hello homepage</h1>
+            <NavbarSimple data={data} />
         </div>
     )
 }
