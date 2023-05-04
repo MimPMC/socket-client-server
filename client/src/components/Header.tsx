ca1db21405304c1dfa77742884a84c7069626c7a
felicia <feliciawillnas@gmail.com>
2023-05-04 10:44:15 +0200

    cat image in header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,12 +5,10 @@ import {
   createStyles,
   Drawer,
   Group,
-  Header
+  Header,
 } from "@mantine/core";
 import { useDisclosure, useMediaQuery } from "@mantine/hooks";
-import { NavbarSimple } from './Sidebar';
-
-
+import { NavbarSimple } from "./Sidebar";
 
 const useStyles = createStyles((theme) => ({
   header: {
@@ -27,8 +25,6 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-
-
 export function HeaderSimple() {
   const [openedDrawer, { toggle: toggleDrawer, close: closeDrawer }] =
     useDisclosure(false);
@@ -39,39 +35,45 @@ export function HeaderSimple() {
   };
 
   const HeaderTitle = {
-    fontSize: "25px",
+    fontSize: "31px",
   };
 
   const data = [
-    { link: '', label: 'Room 1', },
-    { link: '', label: 'Room 2',  },
-    { link: '', label: 'Room 3',  },
-    { link: '', label: 'Room 4',  },
+    { link: "", label: "Room 1" },
+    { link: "", label: "Room 2" },
+    { link: "", label: "Room 3" },
+    { link: "", label: "Room 4" },
   ];
 
-  const isDesktop = useMediaQuery('(min-width: 1024px)');
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
 
   return (
     <>
-    <Header sx={StyledHeader} height={70} mb={120}>
-      <Container className={classes.header}>
-        <Center sx={HeaderTitle} maw={400} h={100} mx="auto">
-          Cat Chat
-        </Center>
-        <Group spacing={5}></Group>
+      <Header sx={StyledHeader} height={70} mb={120}>
+        <Container className={classes.header}>
+          <Center sx={HeaderTitle} maw={400} h={100} mx="auto">
+            <img
+              src="/src/assets/chatcat.jpeg"
+              alt="Random image"
+              height="50px"
+              style={{ borderRadius: "50%", marginRight: "10px" }}
+            />
+            Cat Chat
+          </Center>
+          <Group spacing={5}></Group>
 
-        <Burger
-          opened={openedDrawer}
-          onClick={toggleDrawer}
-          className={classes.burger}
-          size="sm"
-        />
-        <Drawer opened={openedDrawer} onClose={closeDrawer}>
-        {!isDesktop && <NavbarSimple data={data} />}
-        </Drawer>
-      </Container>
-    </Header>
-    {isDesktop && <NavbarSimple data={data} />}
+          <Burger
+            opened={openedDrawer}
+            onClick={toggleDrawer}
+            className={classes.burger}
+            size="sm"
+          />
+          <Drawer opened={openedDrawer} onClose={closeDrawer}>
+            {!isDesktop && <NavbarSimple data={data} />}
+          </Drawer>
+        </Container>
+      </Header>
+      {isDesktop && <NavbarSimple data={data} />}
     </>
   );
 }
