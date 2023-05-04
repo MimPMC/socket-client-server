5f165574c70da3825030635c0fef3d4c5a45a652
mims <113519976+MimPMC@users.noreply.github.com>
2023-05-04 13:10:57 +0200

    Update HomePage.tsx

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,15 +1,13 @@
 import Chat from "../Chat";
- Styling-chat
+
 import { HeaderSimple } from "../Components/Header";
-main
+
 
 export function HomePage() {
     return(
         <div>
            <HeaderSimple />
- Styling-chat
- <Chat/>
- main
+            <Chat/>
         </div>
     )
 }
