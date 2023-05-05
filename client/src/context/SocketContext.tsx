7217b73d138b34fb31381aa2b6fc78173d6c4bdc
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-05 13:11:49 +0200

    socketcontext

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -1,21 +1,35 @@
-import { PropsWithChildren, createContext, useState, useContext, useEffect} from "react";
-import { Socket, io } from "socket.io-client";
+import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
 import type {
-  ClientToServerEvents,
-  ServerToClientEvents,
+  Message
 } from "../../../server/communication";
 
 interface ContextValues {
-  socket: Socket;
+  joinRoom: (room: string, name: string) => void;
+  sendMessage: (message: string) => void;
+  room?: string;
+  messages: Message[];
 }
+
+const socket = io();
 
 const SocketContext = createContext<ContextValues>(null as any);
 export const useSocket = () => useContext(SocketContext);
 
-export function SocketProvider({ children }: PropsWithChildren) {
-  const [socket] = useState<Socket<ServerToClientEvents, ClientToServerEvents>>(
-    io()
-  );
+function SocketProvider({ children }: PropsWithChildren) {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [room, setRoom] = useState<string>();
+
+  const joinRoom = (room: string, name: string) => {
+    socket.emit('join', room, name, () => {
+      setRoom(room);
+    });
+  };
+
+  const sendMessage = (message: string) => {
+    if (!room) throw Error("Can't send message without a room");
+    socket.emit('message', room, message);
+  }
 
   useEffect(() => {
     function connect() {
@@ -24,22 +38,30 @@ export function SocketProvider({ children }: PropsWithChildren) {
     function disconnect() {
         console.log('disconnected from server')  
     }
-    function message(message:string) {
-        console.log(message)  
+    function message(name: string, message: string) {
+        setMessages((messages) => [...messages, { name, message }]);
     }
+    function rooms(rooms: string[]) {
+      console.log(rooms);
+    }
+
     socket.on('connect', connect);
     socket.on('disconnect', disconnect);
     socket.on('message', message)
+    socket.on('rooms', rooms);
     return()=> {
         socket.off('connect', connect)
         socket.off('disconnect', disconnect)
         socket.off('message', message)
+        socket.off('rooms', rooms);
     }
-  }, [socket]);
+  }, []);
 
   return (
-    <SocketContext.Provider value={{ socket }}>
+    <SocketContext.Provider value={{ joinRoom, sendMessage, room, messages }}>
       {children}
     </SocketContext.Provider>
   );
 }
+
+export default SocketProvider;
