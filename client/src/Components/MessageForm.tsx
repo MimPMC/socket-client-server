66d4ef592694d7da733419480b5c4cbf8263e02f
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-05 13:20:09 +0200

    add messageform

---
+++ client/src/Components/MessageForm.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { useSocket } from '../context/SocketContext';
+
+function MessageForm() {
+  const [message, setMessage] = useState('');
+  const { sendMessage } = useSocket();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    sendMessage(message);
+    setMessage('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="message"
+        placeholder="Write a message..."
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+}
+
+export default MessageForm;
