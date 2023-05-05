ecd96b5151530b782112fd32261f992057eb8af4
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-05 13:32:36 +0200

    chat

--- client/src/Chat.tsx
+++ client/src/Chat.tsx
@@ -1,28 +1,12 @@
-import { Alert, Avatar, Badge, Button, Card, Col, Grid, Image, Input, Text } from "@mantine/core";
-import { FormEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Image, Text } from "@mantine/core";
 import "./App.css";
 import clip from "./assets/clip.png";
-import { useUsername } from "./context/UsernameContext";
+import MessageForm from "./Components/MessageForm";
+import { useSocket } from "./context/SocketContext";
+
 
 function Chat() {
-  const { username } = useUsername();
-  const [messages, setMessages] = useState<string[]>([]);
-  const [newMessage, setNewMessage] = useState<string>("");
-  const [showAlert, setShowAlert] = useState<boolean>(false);
-  const navigate = useNavigate();
-
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    if (!username) {
-      setShowAlert(true);
-    } else {
-      setMessages([...messages, newMessage]);
-      setNewMessage("");
-    }
-  }
-  
-
+const { room, messages } = useSocket();  
   return (
     <div
       className="App"
@@ -52,74 +36,20 @@ function Chat() {
         }}
       />
       <div className="chat-header">
-        <Avatar src="" size="xl" radius="md" alt="Avatar" />
         <div className="chat-header-info">
           <Text weight={500} size="md" style={{ marginLeft: "20px" }}>
-            {username}
+            You are in room: {room}
           </Text>
-          <Badge color="gray" variant="light" style={{ marginLeft: "8px" }}>
-            Online
-          </Badge>
         </div>
       </div>
-      <div className="chat-body">
-        {messages.map((message, index) => (
-          <Card
-            style={{
-              borderRadius: "25px",
-              marginRight: "2rem",
-              marginTop: "1rem",
-              width:'70%'
-            }}
-            key={index}
-            className={
-              index % 2 === 0 ? "chat-message-left" : "chat-message-right"
-            }
-          >
-            <Text>{username}: {message}</Text>
-          </Card>
+        <ul>
+        {messages.map((message, i) => (
+          <li key={i}>
+            {message.name}: {message.message}
+          </li>
         ))}
-      </div>
-{/* Add this right above the <form> element */}
-{showAlert && (
-  <Grid gutter="md">
-    <Col>
-      <Alert color="red" title="Error" onClose={() => setShowAlert(false)}>
-        You have to choose a username to chat!
-        <Button
-          color="red"
-          style={{ marginLeft: "10px" }}
-          onClick={() => {
-            setShowAlert(false);
-            navigate("/");
-          }}
-        >
-          Choose a username
-        </Button>
-      </Alert>
-    </Col>
-  </Grid>
-)}
-
-
-      <form
-        className="chat-form"
-        onSubmit={handleSubmit}
-        style={{ bottom: "7.5%", position: "fixed", width: "67%" }}
-      >
-        <Input
-          className="chat-form-input"
-          value={newMessage}
-          onChange={(event) => setNewMessage(event.target.value)}
-          placeholder="Type your message..."
-          rightSection={
-            <Button type="submit" className="chat-form-button">
-              {" "}
-              Send
-            </Button>
-          }
-        />
-      </form>
+      </ul>
+        <MessageForm />
     </div>
   );
 }
