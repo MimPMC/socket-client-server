c80886e5a2654371a25ceeb5d8ca54bc4b3d55ca
mims <113519976+MimPMC@users.noreply.github.com>
2023-05-08 14:10:41 +0200

    Create TestPage.tsx

---
+++ client/src/pages/TestPage.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "../Components/e";
+
+export function TestPage() {
+    
+    return(
+        <div>
+            <h1>Hello</h1>
+            <h3>Hey hey</h3>
+            <Footer></Footer>
+
+
+        </div>
+    )
+}
