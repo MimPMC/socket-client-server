f44d802509361aea207698b7372681bacec783e1
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-11 08:56:00 +0200

    formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,5 @@
-
 import { Outlet } from "react-router-dom";
 import "./App.css";
-//import JoinForm from "./Components/JoinForm";
-//import { useSocket } from "./context/SocketContext";
-//import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,20 +2,14 @@ import { Box } from "@mantine/core";
 import Chat from "../Chat";
 import { HeaderSimple } from "../Components/Header";
 
-
-
-
 export function HomePage() {
+  return (
+    <div>
+      <HeaderSimple></HeaderSimple>
 
-    return(
-        <div>
-            <HeaderSimple></HeaderSimple>
-            
-            <Box w={"100%"} h={"85vh"} bg={"#FEC48F"} >
-            <Chat></Chat>
-            </Box>
-            
-           
-        </div>
-    )
+      <Box w={"100%"} h={"85vh"} bg={"#FEC48F"}>
+        <Chat></Chat>
+      </Box>
+    </div>
+  );
 }
